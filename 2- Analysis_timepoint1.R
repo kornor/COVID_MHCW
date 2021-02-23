f980d017b5f3558033aa74292520cf70b52fefa1
Korinne Northwood <kozimoto@gmail.com>
2021-02-23 20:02:25 +1000

    updated analysis code - still rough

--- 2- Analysis_timepoint1.R
+++ 2- Analysis_timepoint1.R
@@ -11,7 +11,11 @@ library(tidyverse)
 library(plyr)
 library(likert)
 library(ggplot2)
-
+library(ggsignif)
+library (ggpubr)
+library(RColorBrewer)
+library(dplyr)
+library(Hmisc)
 ## read in the data from timepoint 1
 # make strings factors, and force empty cells into NAs
 tp1 <- read.csv("Data_tp1_cleaned.csv", header = TRUE, stringsAsFactors = TRUE, na.strings="")
@@ -35,7 +39,7 @@ tp1$Health_impact <- factor(tp1$Health_impact, levels = c("Very negative impact"
 tp1$Workplace_impact <- factor(tp1$Workplace_impact, levels = c("Very negative impact", "Negative impact", "Little or no impact", "Positive Impact", "Very positive impact"))
 tp1$Flexibility_impact <- factor(tp1$Flexibility_impact, levels = c("Very negative impact", "Negative impact", "Little or no impact", "Positive Impact", "Very positive impact"))
 
-
+tp1$Vulnerable <- factor(tp1$Vulnerable, levels = c("Yes","No", "Unsure"))
 ## descriptive
 ## Age distribution
 summary(tp1$Age)
@@ -46,8 +50,15 @@ summary(tp1$Gender)
 
 ## Occupational discpline
 summary(tp1$Discipline)
+summary(tp1$Medical_role)
 
-# Medical Role
+# testing
+summary(tp1$Vulnerable)
+summary(tp1$Days_absent)
+summary(tp1$Number_tests)
+mode(tp1$Number_tests)
+describe(tp1$Days_absent)
+describe(tp1$Number_tests)
 
 ## Site
 
@@ -68,9 +79,51 @@ summary(tp1$Anxiety_mar)
 ggplot(tp1, aes(x = Vulnerable, y = Anxiety_mar)) + geom_boxplot()
 ggplot(tp1, aes(x = Vulnerable, y = Anxiety_jan)) + geom_boxplot()
 
-# pivot
+ggplot(tp1, aes(Anxiety_jan)) + geom_bar()
 
 
+# pivot / gather
+# The arguments to gather():
+# - data: Data object
+# - key: Name of new key column (made from names of data columns)
+# - value: Name of new value column
+# - ...: Names of source columns that contain values
+# - factor_key: Treat the new key column as a factor (instead of character vector)
+data_long <- gather(tp1, Timepoint, Anxiety, Anxiety_mar:Anxiety_jan, factor_key=TRUE)
+
+# boxplot 
+levels()
+p <- ggplot(data_long, aes(x=Timepoint, y=Anxiety, fill = Vulnerable)) + 
+  geom_boxplot() +
+  geom_signif(comparisons = list(c("Anxiety_mar", "Anxiety_jan")), map_signif_level=TRUE) 
+  #geom_signif(stat = identity, map_signif_level=TRUE)
+p
+
+plot <- ggplot(data_long, aes(Timepoint, Anxiety)) +
+  geom_bar(aes(fill = Vulnerable), stat="identity", position="dodge", width=.5) +
+  geom_signif(stat="identity",
+              data=data.frame(x=c(0.875, 1.875), xend=c(1.125, 2.125),
+                              y=c(105, 95), annotation=c("**", "NS")),
+              aes(x=x,xend=xend, y=y, yend=y, annotation=annotation)) +
+  geom_signif(comparisons=list(c("S1", "S2")), annotations="***",
+              y_position = 125, tip_length = 0, vjust=0.4) +
+  scale_fill_manual(values = c("grey80", "grey20"))
+plot
+
+# "p.signif" is stars
+# p.format = the value
+# no comment, the test and the value
+
+p <- ggplot(data_long, aes(x=Timepoint, y=Anxiety, fill = Vulnerable)) + 
+  geom_boxplot() +
+  stat_compare_means(comparisons = list(c("Anxiety_mar", "Anxiety_jan")),label.y = 120) +
+  #stat_compare_means() +
+  stat_compare_means(aes(group = Vulnerable),label = "p.signif", label.y = c(105, 100))
+  
+  p
+
+
+## K-w analysis 
 
 ## vulnerability leads to persistence of anxiety?
 
@@ -78,8 +131,12 @@ ggplot(tp1, aes(x = Vulnerable, y = Anxiety_jan)) + geom_boxplot()
 
 
 ## main concerns
-
-
+summary(tp1$Risk.of.becoming.infected.in.the.workplace)
+summary(tp1$Risk.of.passing.infection.onto.others..such.as.family.)
+summary(tp1$Concern.about.PPE.or.working.conditions)
+summary(tp1$Increased.workload)
+summary(tp1$Increased.acuity...distress.of.clients)
+summary(tp1$Fear.of.redeployment.to.other.services)
 
 
 #### likert 
@@ -130,8 +187,8 @@ ggplot(tp1, aes(x = Vulnerable, y = Anxiety_jan)) + geom_boxplot()
 
 
 
-ggplot(tp1, aes(x  = A_dass_score, y = Anxiety_mar)) + geom_boxplot()
-ggplot(tp1, aes(x  = A_dass_score, y = Anxiety_jan)) + geom_boxplot()
+ggplot(tp1, aes(x  = A_dass_score, y = Anxiety_mar)) + geom_boxplot() + stat_compare_means()
+ggplot(tp1, aes(x  = A_dass_score, y = Anxiety_jan)) + geom_boxplot() + stat_compare_means()
 
 
 ggplot(tp1, aes(x  = P_cbi_score, y = Anxiety_mar)) + geom_boxplot()
@@ -139,9 +196,9 @@ ggplot(tp1, aes(x  = P_cbi_score, y = Anxiety_jan)) + geom_boxplot()
 ggplot(tp1, aes(x  = W_cbi_score, y = Anxiety_mar)) + geom_boxplot()
 
 ### discpline
-ggplot(tp1, aes(x  = Discipline, y = P_cbi_val)) + geom_boxplot()
-ggplot(tp1, aes(x  = Discipline, y = W_cbi_val)) + geom_boxplot()
-ggplot(tp1, aes(x  = Discipline, y = C_cbi_val)) + geom_boxplot()
+ggplot(tp1, aes(x  = Discipline, y = P_cbi_val)) + geom_boxplot() + stat_compare_means()
+ggplot(tp1, aes(x  = Discipline, y = W_cbi_val)) + geom_boxplot() + stat_compare_means()
+ggplot(tp1, aes(x  = Discipline, y = C_cbi_val)) + geom_boxplot() + stat_compare_means()
 
 ggplot(tp1, aes(x  = Discipline, y = D_dass_val)) + geom_boxplot()
 ggplot(tp1, aes(x  = Discipline, y = A_dass_val)) + geom_boxplot()
@@ -180,3 +237,40 @@ ggplot(tp1, aes(x  = Days_absent, y = A_dass_val)) + geom_point()
 ggplot(tp1, aes(x  = Days_absent, y = W_cbi_val)) + geom_point()
 
 ggplot(tp1, aes(x  = Number_tests, y = A_dass_val)) + geom_point()
+
+
+
+### Stacked bar of DASS severeity percentages
+
+# long
+df_long <- gather(tp1, Dimension, Score, c(29,31,33), factor_key = TRUE)
+### set the levels again for "Score"
+df_long$Score <- factor(df_long$Score, levels = c("Extremely severe", "Severe", "Moderate", "Mild", "Normal"))
+
+
+
+##
+ggplot(df_long %>% count(Dimension, Score) %>%    # Group by dimension and score, then count number in each group
+         mutate(pct=n/sum(n),               # Calculate percent within each region
+                ypos = cumsum(n) - 0.5*n),  # Calculate label positions
+       aes(Dimension, n, fill=Score)) +
+  geom_bar(stat="identity") +
+  scale_fill_brewer(palette="Spectral") +
+  geom_text(aes(label=paste0(sprintf("%1.1f", pct*100),"%")), 
+            position=position_stack(vjust=0.5))
+
+
+#### and for the CBI
+df_long <- gather(tp1, Dimension, Score, c(35,37,39), factor_key = TRUE)
+
+
+df_long$Score <- factor(df_long$Score, levels = c("Severe","High", "Moderate", "Low"))
+
+ggplot(df_long %>% count(Dimension, Score) %>%    # Group by dimension and score, then count number in each group
+                mutate(pct=n/sum(n),               # Calculate percent within each region
+                ypos = cumsum(n) - 0.5*n),  # Calculate label positions
+       aes(Dimension, n, fill=Score)) +
+  geom_bar(stat="identity") +
+  scale_fill_brewer(palette="Spectral") +
+  geom_text(aes(label=paste0(sprintf("%1.1f", pct*100),"%")), ## positions the percent labels
+            position=position_stack(vjust=0.5))
